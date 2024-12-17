3977f3a02af39b88a34fafed1c3a1ae0a4c9caf4
breno <brenobritobc@gmail.com>
2024-12-17 13:44:44 -0300

    inicializando base projeto

---
+++ src/pages/Cadastro.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './Cadastro.css';
+import Button from "../components/Button";
+
+function Cadastro(){
+    return <div id="cadastro">
+        <Button nome="Cadastrar"/>
+    </div>
+}
+
+export default Cadastro;
---
+++ src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './Login.css';
+import Button from "../components/Button";
+
+function Login(){
+    return <div id="login">
+        <Button nome="Entrar"/>
+    </div>
+
+}
+
+export default Login;
