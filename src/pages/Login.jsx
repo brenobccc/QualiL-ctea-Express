ec8b1a6d1609e122a29eeb344712b0cf596d9761
breno <brenobritobc@gmail.com>
2024-12-17 14:47:09 -0300

    finalizando tela de login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,9 +2,37 @@ import React from "react";
 import './Login.css';
 import Button from "../components/Button";
 
-function Login(){
+function Login() {
     return <div id="login">
-        <Button nome="Entrar"/>
+        {/* <Button nome="Entrar"/> */}
+        <form action="/cadastro" method="get">
+            <div id="login-inputs">
+                <input placeholder="E-mail" type="email">
+                </input>
+                <input placeholder="Senha" type="password">
+                </input>
+            </div>
+            <div id="login-forgot-password">
+                <a href="/cadastro">Esqueci a senha</a>
+            </div>
+            <div id="login-button">
+                <button type="submit">ENTRAR</button>
+            </div>
+            <div id="login-create-account">
+                Não tem conta?<a href="/cadastro">  Cadastre-se</a>
+            </div>
+            {/* <div>
+                <label for="email">E-mail:</label>
+                <input type="email" id="email" />
+            </div>
+            <div>
+                <label for="senha" >Mensagem:</label>
+                <input id="senha" type="password"></input>
+            </div>
+            <div class="button">
+                <button type="submit">Enviar sua mensagem</button>
+            </div> */}
+        </form>
     </div>
 
 }
